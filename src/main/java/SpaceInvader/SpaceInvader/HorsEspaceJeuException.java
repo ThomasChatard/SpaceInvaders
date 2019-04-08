76f22fdfad2e554c0f53bdff8504eb5cfb0e7b74
Thomas Chatard <thomas.chatard@etu.unilim.fr>
2019-04-08 17:29:10 +0200

    Commit 1er TP

---
+++ src/main/java/SpaceInvader/SpaceInvader/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package SpaceInvader.SpaceInvader;
+
+@SuppressWarnings("serial")
+class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
