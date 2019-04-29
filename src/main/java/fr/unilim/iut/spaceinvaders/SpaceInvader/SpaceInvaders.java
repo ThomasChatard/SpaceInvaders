3f77334e9cebf4aeb6bcfb86a21ae03809b732d3
Thomas Chatard <thomas.chatard@etu.unilim.fr>
2019-04-29 11:24:18 +0200

    Refactoring du toString()

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvader/SpaceInvaders.java
@@ -15,11 +15,6 @@ public class SpaceInvaders {
 	   this.longueur = longueur;
 	   this.hauteur = hauteur;
    }
-    
-	@Override
-	public String toString() {
-		return recupererEspaceJeuDansChaineASCII();
-	}
 
 	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
